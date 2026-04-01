6152420fe24642e8dd8878aa150885a4f69eb1c8
navedAl97 <navedalam278@gmail.com>
2026-04-01 22:10:51 +0530

    Demo

---
+++ assets/main.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToShow: 3,
+    dots: true,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+    responsive: [
+      {
+
+        breakpoint: 768, // mobile
+        settings: {
+          slidesToShow: 1,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 992, // tablet
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+});
+
+// Slick version 1.5.8
